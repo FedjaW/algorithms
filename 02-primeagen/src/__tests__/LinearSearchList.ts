36c8cb577f89d2c81acb92acd6b6b410bcb15a5d
FedjaW <f.wilms89@web.de>
2024-03-27 20:43:39 +0100

    housekeeping

--- 02-primeagen/src/__tests__/LinearSearchList.ts
+++ 02-primeagen/src/__tests__/LinearSearchList.ts
@@ -1,4 +1,4 @@
-import linear_fn from "@code/LinearSearchList";
+import linear_fn from "../day1/LinearSearchList";
 
 test("linear search array", function () {
     const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
