2f48e2fb277698e89be65ee11c653e4beee6ac8f
Friedrich Wilms <f.wilms89@web.de>
2024-03-29 17:49:45 +0100

    more algo

--- 02-primeagen/src/day1/BTPreOrder.ts
+++ 02-primeagen/src/day1/BTPreOrder.ts
@@ -1,1 +1,29 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {}
+type BinaryNode<T> = {
+    value: T
+    left: BinaryNode<T>
+    right: BinaryNode<T>
+}
+
+function walk(curr: BinaryNode<number> | null, path: number[]): void {
+    if (!curr) {
+        return
+    }
+
+    // do the 3 stepf of a recursion
+    
+    //  1. pre
+    path.push(curr.value)
+    
+    //  2. recurse
+    walk(curr.left, path)
+    walk(curr.right, path)
+
+    //  3. post
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] =  []
+    walk(head, [])
+    
+    return path
+}
--- 02-primeagen/src/day1/QuickSort.ts
+++ 02-primeagen/src/day1/QuickSort.ts
@@ -1,21 +1,42 @@
 
 function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return
+    }
 
+    const pivotIdx = partition(arr, lo, hi)
 
+    qs(arr, lo, pivotIdx - 1)
+    qs(arr, pivotIdx + 1, hi)
 }
 
 function partition(arr: number[], lo: number, hi: number): number {
-// return: what is the pivot inex
     const pivot = arr[hi] // this can cause the worst case. this can be chosen freely
 
-    let idx = -1
-    
+    let idx = lo - 1
 
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++
+            // swap
+            const tmp = arr[i]
+            arr[i] = arr[idx]
+            arr[idx] = tmp
+        }
+    }
+    
+    // move pivot, so that everythin to the left
+    // is smaller and everythin to the right
+    // is bigger than the pivot
+    idx++
+    arr[hi] = arr[idx]
+    arr[idx] = pivot
+
+    return idx // return the pivot index
 }
 
 
 export default function quick_sort(arr: number[]): void {
 
     qs(arr, 0, arr.length - 1) // inclusive [lo, hi]
-    
 }
